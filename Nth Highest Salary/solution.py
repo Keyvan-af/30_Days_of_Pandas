17e976bcd2af467a17a47c182607165a72a93b08
Keyvan-af <70849668+Keyvan-af@users.noreply.github.com>
2024-06-06 20:36:54 +0330

    Solve more challenges

---
+++ Nth Highest Salary/solution.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    unique_salaries = employee['salary'].drop_duplicates()
+    sorted_salaries = unique_salaries.sort_values(ascending=False)
+    if N > len(sorted_salaries):
+        return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
+    elif N <= 0 :
+        return pd.DataFrame({f'getNthHighestSalary({N})': [None]})
+    else:
+        nth_highest = sorted_salaries.iloc[N - 1]
+        return pd.DataFrame({f'getNthHighestSalary({N})' : [nth_highest]})
